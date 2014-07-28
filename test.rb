507cfe535a14768ddd8fa35e2e1e7a97b0eda575
Rafael <sMecKs.rb@gmail.com>
2014-07-27 21:40:49 -0700

    Testing this message thing

--- test.rb
+++ test.rb
@@ -6,13 +6,28 @@ class Student
 		@last_name = last_name
 		@grade = grade
 	end
+
+	def senior?
+		grade == 12
+	end
+
+	def to_s
+		"#{last_name}, #{first_name}"
+	end
 end
 
+
+def seniors(students)
+	students.select { |student| student.senior? }
+end
 
 fred = Student.new("Fred", "James", 12)
 sarah = Student.new("Sarah", "Smith", 12)
 jack = Student.new("Jack", "Gong", 11)
-
 all_students = [fred, sarah, jack]
 
-puts all_students.inspect
+seniors(all_students).each do |student|
+	puts student
+end
+
+puts "test"
